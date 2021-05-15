c6683089fe8d5fa6b8e4607da9dadcdda14e838d
Matt <darknesswish7@gmail.com>
2021-05-15 14:37:19 +0700

    update Profile.vue dan masukkan icons

---
+++ components/pages/store/dashboards/Profile.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="grid grid-flow-row px-8 py-4 gap-8">
+      <div class="flex gap-4 items-center">
+        <img src="/medias/logos/brands/kenzo.png" class="w-32 h-32 object-contain bg-white rounded-full">
+        <div class="flex flex-col">
+          <span class="text-maroon text-2xl font-bold">Kenzo</span>
+          <span>Jakarta Barat, Indonesia</span>
+          <span>bergabung 2 tahun lalu</span>
+        </div>
+      </div>
+      <form action="">
+       <div class="grid grid-flow-row gap-4">
+         <div class="grid grid-cols-3 gap-4">
+           <div class="grid col-span-2 gap-2" >
+             <label class="block lg:text-lg">Nama Toko</label>
+             <input type="text" class="rounded-full py-3 px-6 w-full bg-transparent focus:outline-none border border-dark-grey">
+           </div>
+           <div class="grid gap-2">
+             <label class="block lg:text-lg">No Tlp.</label>
+             <input type="text" class="w-full rounded-full py-3 px-6 bg-transparent focus:outline-none border border-dark-grey" >
+           </div>
+         </div>
+         <div class="grid grid-flow-col gap-4">
+           <div class="grid gap-2">
+             <label class="block lg:text-lg">Email</label>
+             <input type="text" class="rounded-full py-3 px-6 w-full bg-transparent focus:outline-none border border-dark-grey"  >
+           </div>
+           <div class="grid gap-2">
+             <label class="block lg:text-lg">Alamat</label>
+             <input type="text" class="rounded-full py-3 px-6 w-full bg-transparent focus:outline-none border border-dark-grey" >
+           </div>
+         </div>
+         <div class="grid gap-2">
+           <label class="block lg:text-lg">Deskripsi Toko</label>
+           <textarea rows="5" style="resize: none" class="p-4 w-full rounded-2xl bg-transparent focus:outline-none border border-dark-grey"></textarea>
+         </div>
+         <div class="grid grid-cols-2 items-center gap-4">
+           <button class="rounded-full border-2 border-grey py-2 px-6 w-1/2 justify-self-end focus:outline-none">Simpan</button>
+           <button class="rounded-full border-2 border-grey py-2 px-6 w-1/2 justify-self-start bg-dark-grey text-white focus:outline-none">Edit</button>
+         </div>
+       </div>
+      </form>
+
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator';
+
+@Component
+export default class ProfileComponent extends Vue {
+
+}
+</script>
+
--- pages/store/index.vue
+++ pages/store/index.vue
@@ -1,21 +1,23 @@
 <template>
   <main>
-    <div class="p-24 grid grid-cols-4 gap-12" >
-      <div class="py-4 bg-light-grey rounded-4xl">
+    <div class="p-16 grid grid-cols-4 gap-12" >
+      <div class="py-12 bg-light-grey rounded-4xl">
         <div class="grid grid-rows-12 px-12">
           <div class="row-span-8 flex flex-col justify-center items-center gap-4 ">
-            <img src="/medias/logos/brands/kenzo.png" class="w-32 h-32 object-contain bg-white rounded-full">
+            <img src="/medias/logos/brands/kenzo.png" class="w-32 h-32 object-contain bg-white rounded-full" @click="changeDashboard('profile')">
             <span class="text-maroon text-2xl font-bold">Kenzo</span>
-            <button class="rounded-full border-2 border-grey py-2 px-6 w-full">Home</button>
-            <button class="rounded-full border-2 border-grey py-2 px-6 w-full">Pesanan</button>
-            <button class="rounded-full border-2 border-grey py-2 px-6 w-full">Etalase Toko</button>
-            <button class="rounded-full border-2 border-grey py-2 px-6 w-full">Data Transaksi</button>
+            <button class="rounded-full border-2 border-grey py-2 px-6 w-full focus:outline-none" @click="changeDashboard('home')">Home</button>
+            <button class="rounded-full border-2 border-grey py-2 px-6 w-full focus:outline-none" @click="changeDashboard('home')">Pesanan</button>
+            <button class="rounded-full border-2 border-grey py-2 px-6 w-full focus:outline-none" @click="changeDashboard('home')">Etalase Toko</button>
+            <button class="rounded-full border-2 border-grey py-2 px-6 w-full focus:outline-none" @click="changeDashboard('home')">Data Transaksi</button>
           </div>
           <button class="row-start-12 rounded-full border-2 border-grey py-2 px-6 w-full">Logout</button>
         </div>
       </div>
-      <div class="p-8 bg-light-grey col-span-3 rounded-lg">
-
+      <div class="p-8 bg-light-grey col-span-3 rounded-4xl">
+        <keep-alive>
+          <component v-bind:is="dashboard" />
+        </keep-alive>
       </div>
     </div>
   </main>
@@ -27,8 +29,27 @@
 
 <script lang="ts">
 import { Vue, Component, Prop } from 'nuxt-property-decorator';
-@Component
+import ProfileComponent from '../../components/pages/store/dashboards/Profile.vue';
+
+@Component({
+  components: {
+    ProfileComponent,
+  }
+})
 export default class StorePage extends Vue{
+  public dashboard = ProfileComponent
+
+  changeDashboard(dashboard:string){
+    switch (dashboard) {
+      case 'profile':
+        this.dashboard= ProfileComponent;
+        break;
+      default:
+        this.dashboard=ProfileComponent;
+        break;
+    }
+
+  }
 
 }
 </script>
